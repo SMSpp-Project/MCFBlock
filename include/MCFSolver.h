b8c1fe577ae0ff2d6b9f7da09b4681ca276a1d12
Antonio Frangioni <frangio@di.unipi.it>
2020-02-27 19:00:28 +0100

    eliminated OPT_USE_NAMESPACE

--- include/MCFSolver.h
+++ include/MCFSolver.h
@@ -11,9 +11,9 @@
  * template over the underlying :MCFClass object, which implies that most of
  * the code is in the header file.
  *
- * \version 1.11
+ * \version 1.12
  *
- * \date 16 - 09 - 2019
+ * \date 27 - 02 - 2020
  *
  * \author Antonio Frangioni \n
  *         Operations Research Group \n
@@ -44,9 +44,7 @@
 /// namespace for the Structured Modeling System++ (SMS++)
 namespace SMSpp_di_unipi_it
 {
- #if( OPT_USE_NAMESPACES )
-  using namespace MCFClass_di_unipi_it;
- #endif
+ using namespace MCFClass_di_unipi_it;
 
 /*--------------------------------------------------------------------------*/
 /*------------------------------- CLASSES ----------------------------------*/
