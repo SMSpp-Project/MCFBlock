404856a251435dce3e61c4816a552cf0b52966f4
Donato Meoli <donato.meoli.95@gmail.com>
2024-04-07 11:11:24 +0200

    fixed cmake file if cplex is not available

--- test/test.cpp
+++ test/test.cpp
@@ -45,7 +45,9 @@
  * - 5      for the SPTree class; note that SPTree cannot solve
  *          most MCF instances, except those with SPT structure */
 
-#define WHICH_MCF 1
+#ifndef WHICH_MCF
+ #define WHICH_MCF 1
+#endif
 
 #if( LOG_LEVEL >= 1 )
 #define LOG1( x ) cout << x
